d75054187c207aaa0ea5fb827e2d947b339ef1c9
SimbaRafo <mihaela.cahun@gmail.com>
2025-05-17 13:55:46 +0200

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,4 @@
+def on_logo_pressed():
+    if True:
+        basic.show_icon(IconNames.HEART)
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
---
+++ main.ts
@@ -0,0 +1,3 @@
+while (input.logoIsPressed()) {
+	
+}
